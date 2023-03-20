cb8d11e3ea5e414237b4d2b10a2cc7c984fc0b75
MichaelKhaykin <mrkhaykin@gmail.com>
2023-03-19 22:04:03 -0700

    gpt: updating files

--- animals/animal/animal.py
+++ animals/animal/animal.py
@@ -2,3 +2,6 @@ class Animal:
 	def __init__(self, name, age):
 		self.name = name
 		self.age = age
+		
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name='{self.name}', age={self.age})"
