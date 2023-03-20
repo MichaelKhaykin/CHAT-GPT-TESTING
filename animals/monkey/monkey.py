0d71d67cae59efaeed0656914e13d5d94c7ad44b
MichaelKhaykin <mrkhaykin@gmail.com>
2023-03-19 22:04:07 -0700

    gpt: updating files

--- animals/monkey/monkey.py
+++ animals/monkey/monkey.py
@@ -1,7 +1,6 @@
 from animals import Animal
 class Monkey(Animal):
-    def __init__(self, name, age, amountOfBananasEaten, hopDistance):
+    def __init__(self, name, age, amountOfBanannasEaten, hopDistance):
         super().__init__(name, age)
-        self.amountOfBananasEaten = amountOfBananasEaten
+        self.amountOfBanannasEaten = amountOfBanannasEaten
         self.hopDistance = hopDistance
-        
